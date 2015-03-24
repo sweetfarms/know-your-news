ec93f165232ad5e2fb70f62bd3e19e18161418f0
kssweet1 <ks@kylesweet.com>
2015-03-24 18:05:46 -0400

    had to make sacrifice on second relevant article api call - did not work as planned, relevant search through nytimes.com

--- app/helpers/nytimes_helper.rb
+++ app/helpers/nytimes_helper.rb
@@ -3,8 +3,9 @@ module NytimesHelper
   class NYTimes
     @@url_base  = "http://api.nytimes.com/svc/news/v3/content/nyt/world.json?api-key="
     @@client_id = Rails.application.secrets.nytimes_top_id
-    @@search_url_base = "http://api.nytimes.com/svc/search/v2/articlesearch.json?fq="
-    @@search_id = Rails.application.secrets.nytimes_search_id
+    @@search_url_base = "http://query.nytimes.com/search/sitesearch/?action=click&contentCollection&region=TopBar&WT.nav=searchWidget&module=SearchSubmit&pgtype=Homepage#/"
+    # @@search_url_base = "http://api.nytimes.com/svc/search/v2/articlesearch.json?fq="
+    # @@search_id = Rails.application.secrets.nytimes_search_id
 
     def self.retrieve_articles
       NYTimes.build_article_hash
@@ -16,32 +17,36 @@ module NytimesHelper
     end
 
     def self.build_article_hash
-      # first we build a hash where the keys will be the sections 
+      # build a hash where the keys will be the sections 
       articles = {}
 
       NYTimes.articles.each do |article|
 
-        # the first time we have a new section, it won't be a key yet,
+        # the first time there is a new section, it won't be a key yet,
         # so, if the hashes key doesn't exit, create it and make it an
         # empty array
         if articles[article["subsection"]].nil?
           articles[article["subsection"]] = []
         end
 
-        # then, push the article on to the existing sections' array
+
+        # then, article is pushed on to the existing sections' array
         articles[article["subsection"]].push({
           :title             => article["title"],
           :abstract          => article["abstract"],
           :link              => article["url"],
           :geographic_facet  => article["geo_facet"],
           :description_facet => article["des_facet"],
-          :relevant_articles => @@search_url_base + article["title"].split.join('+').gsub(/\s+/, "+").downcase + "&fq=" + article["geo_facet"].split.join('+').gsub(/\s+/, "+").downcase + "&api-key=" + @@search_id
-          
+          :relevant_articles => @@search_url_base + article["title"].split.join('+').gsub(/\W/, "+").squeeze(" ").downcase
+            
         })
-        end
-      return articles
+          end
+        return articles
+      end
     end
-  end
+
+    #faded dreams below
+    # news = HTTParty.get(@@search_url_base + article["title"].split.join('+').gsub(/\W/, "+").squeeze(" ").downcase + "&fq=" + article["geo_facet"].split.join('+').gsub(/\W/, "+").squeeze(" ").downcase + "&api-key=" + @@search_id)
 
 
 end
