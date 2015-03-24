e62b0d3aeebd77b05d2f5ef36ce18526ca0d3c56
kssweet1 <ks@kylesweet.com>
2015-03-24 11:21:37 -0400

    duplicate news articles will no longer be saved

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -7,18 +7,22 @@ class NewsController < ApplicationController
 
   def create
     @news = News.new(user_id: current_user.id, title: params[:title], abstract: params[:abstract], article_url: params[:article_url])     
+
     respond_to do |format|
       if @news.save
         format.html  { redirect_to(@news,
-                      :notice => 'Post was successfully created.') }
+                      :notice => 'Article saved!') }
         format.json  { render :json => @news,
                       :status => :created, :location => @news }
       else
-        format.html  { render :action => "new" }
+        format.html  { redirect_to(@news,
+                      :notice => 'You have already saved that article.')
+                     }
         format.json  { render :json => @news.errors,
                       :status => :unprocessable_entity }
       end
     end
+
   end
   
 
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,3 +1,4 @@
 class News < ActiveRecord::Base
   belongs_to :user
+  validates_uniqueness_of :title, scope: :user_id
 end
